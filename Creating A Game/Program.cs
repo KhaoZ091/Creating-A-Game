13e54b7d4a82f8e86cf21cef90f150c572789dd9
cyrus <cyrus@DESKTOP-BMHD6JG>
2024-02-01 16:58:19 +0800

    Add project files.

---
+++ Creating A Game/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Xml.Serialization;
+
+namespace Creating_A_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+
+            Console.WriteLine("Ephemeral Reckoning: The Lich's Curse");
+            Console.WriteLine(     "Press Any Enter to continue");
+            Console.ReadKey();
+        }
+    }
+
+    
